6f2c407c9182a9a0893c6853e9249183b8070a55
Muhammad Abdullah <callmeab1182002@gmail.com>
2026-04-02 02:05:09 +0500

    corps added and few changes in main.py

--- api/main.py
+++ api/main.py
@@ -4,23 +4,29 @@ import numpy as np
 from pathlib import Path
 import uvicorn
 import os
+from fastapi.middleware.cors import CORSMiddleware
 
-# -----------------------------
-# Define FastAPI app first
-# -----------------------------
 app = FastAPI()
 
-# -----------------------------
-# Build absolute paths for models
-# -----------------------------
-BASE_DIR = Path(__file__).resolve().parent.parent  # points to smart-electricity-ai/
+# CORS (important for Streamlit)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Fix path
+BASE_DIR = Path(__file__).resolve().parent
+
+print("Loading models...")
 
 model = joblib.load(BASE_DIR / "models" / "model.pkl")
 anomaly_model = joblib.load(BASE_DIR / "models" / "anomaly.pkl")
 
-# -----------------------------
-# API Endpoints
-# -----------------------------
+print("Models loaded successfully ✅")
+
 @app.get("/")
 def home():
     return {"message": "AI Electricity API Running"}
@@ -36,9 +42,6 @@ def detect(value: float):
     result = anomaly_model.predict([[value]])[0]
     return {"anomaly": int(result)}
 
-# -----------------------------
-# Run the server (only if main)
-# -----------------------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("api.main:app", host="0.0.0.0", port=port)
